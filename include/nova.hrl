1f0844d96fca0dea870d8f3ada29e4b5a6089f48
Niclas Axelsson <niclas@burbas.se>
2020-02-15 21:50:49 +0100

    Bind the Level variable in a function

--- include/nova.hrl
+++ include/nova.hrl
@@ -1,12 +1,22 @@
+-define(TERM_RED, "\033[31m").
+-define(TERM_GREEN, "\033[32m").
+-define(TERM_YELLOW, "\033[33m").
+-define(TERM_BLUE, "\033[34m").
+-define(TERM_BOLD, "\033[1m").
+-define(TERM_RESET, "\033[m").
+
 -ifndef(OTP_RELEASE).
 -define(LOG(Level, Msg), io:format("[~p] ~s~n", [Level, Msg])).
 -define(LOG(Level, Msg, Metadata),
-        case Level of
-            error -> io:format(?TERM_RED);
-            warning -> io:format(?TERM_YELLOW);
-            info -> io:format(?TERM_GREEN);
-            _ -> ok
-        end,
+        S = fun(L) ->
+                    case L of
+                        error -> io:format(?TERM_RED);
+                        warning -> io:format(?TERM_YELLOW);
+                        info -> io:format(?TERM_GREEN);
+                        _ -> ok
+                    end
+            end,
+        S(Level),
         io:format("[~p] ", [Level]),
         io:format(Msg, Metadata),
         io:format("~s~n", [?TERM_RESET])).
@@ -16,14 +26,6 @@
 -define(LOG(Level, Msg, Metadata), logger:log(Level, Msg, Metadata)).
 -define(WITH_STACKTRACE(T, R, S), T:R:S ->).
 -endif.
-
-
--define(TERM_RED, "\033[31m").
--define(TERM_GREEN, "\033[32m").
--define(TERM_YELLOW, "\033[33m").
--define(TERM_BLUE, "\033[34m").
--define(TERM_BOLD, "\033[1m").
--define(TERM_RESET, "\033[m").
 
 -define(DEBUG(M), ?LOG(debug, M)).
 -define(DEBUG(M, Meta), ?LOG(debug, M, Meta)).
